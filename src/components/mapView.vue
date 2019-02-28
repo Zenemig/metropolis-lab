dd0a448ee7feb134fd3753c84f538c0a5a8ef55e
Zenemig <hola@zenemig.net>
2019-02-28 02:18:24 -0300

    feat(map): remove unnecesary map controls

--- src/components/mapView.vue
+++ src/components/mapView.vue
@@ -16,7 +16,13 @@ export default {
     const element = document.querySelector('.c-map')
     const options = {
       zoom: 5,
-      center: new google.maps.LatLng(41.3851, 2.1734)
+      center: new google.maps.LatLng(41.3851, 2.1734),
+      zoomControl: true,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      rotateControl: true,
+      fullscreenControl: true
     }
     const map = new google.maps.Map(element, options)
   },
